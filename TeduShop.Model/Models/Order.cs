366c9c4a822f264acab729a5c0e0db603e39afac
THANHBINH\THANHBINH <thanhbinh545@gmail.com>
2018-08-28 09:50:35 +0700

    add domain entities fro project

---
+++ TeduShop.Model/Models/Order.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Orders")]
+    public class Order : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string CustomerName { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string CustomerAddress { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string CustomerEmal { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string CustomerMobile { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string CustomerMessage { set; get; }
+
+        [MaxLength(256)]
+        public string PatmentMethod { set; get; }
+
+        [Required]
+        public string PaymentStatus { set; get; }
+
+        public virtual IEnumerable<OrderDetail> OrderDetails { set; get; }
+    }
+}
